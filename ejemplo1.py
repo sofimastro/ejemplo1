c342da0d8826223c973eb95aa5d39b3798f997c8
sofimastro <sofiamastronardi@Air-de-Sofia.fibertel.com.ar>
2024-07-04 19:45:42 -0300

    agrego funcion mensaje2

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,11 +1,12 @@
 def mensaje():
     print("estoy trabajando")
     
-    mensaje()
+mensaje()
     
-    def sumar(n1,n2):
-        print("el resultado de la suma entre n1 + n2 es de: ", n1+n2)
+def sumar(n1,n2):
+         print("el resultado de la suma entre n1 + n2 es de: ", n1+n2)
         
-    sumar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: "))
-          
-    
+sumar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: "))
+      
+def mensaje2():
+    print("volvo al pasado")
--- ejemplo2.py
+++ ejemplo2.py
@@ -1,4 +1,4 @@
 def resta(n1,n2):
         print("el resultado de la resta entre n1 +- n2 es de: ", n1-n2)
         
-    sumar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: "))
+sumar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: "))
