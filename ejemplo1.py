32309c179949da7f002e12464c0d94438b7277aa
sofimastro <sofiamastronardi@Air-de-Sofia.fibertel.com.ar>
2024-07-04 19:34:03 -0300

    agrego la funcion sumar y restar

--- ejemplo1.py
+++ ejemplo1.py
@@ -2,3 +2,10 @@ def mensaje():
     print("estoy trabajando")
     
     mensaje()
+    
+    def sumar(n1,n2):
+        print("el resultado de la suma entre n1 + n2 es de: ", n1+n2)
+        
+    sumar(int(input("ingresa el primer numero: ")),int(input("ingresa el segundo numero: "))
+          
+    
